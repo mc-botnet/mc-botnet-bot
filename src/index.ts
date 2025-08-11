1a06fc289f03a1bb10d3e0eb38fdd242e6af614d
bakane <dyatlovleonid@gmail.com>
2025-08-11 19:04:21 +0300

    TODO pass errors to server

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,40 @@ import { Rpc } from "./rpc.ts";
 async function main() {
     const log = pino();
 
-    log.info("AAAAAAa");
-
     const config = {
         botId: process.env.BOT_ID!,
         grpcHost: process.env.GRPC_HOST!,
         grpcPort: process.env.GRPC_PORT!,
     };
 
-    const rpc = new Rpc(config);
+    const rpc = new Rpc(config, log);
 
     const server = createServer();
     server.add(BotDefinition, rpc);
+
     const port = await server.listen("localhost:0");
     log.info({ port: port }, "Started listening");
 
     const channel = createChannel(`${config.grpcHost}:${config.grpcPort}`);
-
     log.info("Connected to gRPC channel");
 
     const acceptorClient: AcceptorClient = createClient(AcceptorDefinition, channel);
-
     await acceptorClient.ready({
         id: config.botId,
         port: port,
     });
     log.info("Sent Ready request");
+
+    await waitForSignal();
+    log.info("Shutting down");
+    rpc.close();
+}
+
+function waitForSignal(): Promise<void> {
+    return new Promise<void>((resolve) => {
+        process.on("SIGINT", () => resolve());
+        process.on("SIGTERM", () => resolve());
+    });
 }
 
 await main();
--- src/rpc.ts
+++ src/rpc.ts
@@ -1,23 +1,37 @@
-import type { CallContext, ServiceImplementation } from "nice-grpc";
+import { type CallContext, ServerError, type ServiceImplementation, Status } from "nice-grpc";
 import type { DeepPartial, PingResponse, BotDefinition, Event, ConnectRequest } from "./pb/bot.ts";
 import type { Empty } from "./pb/google/protobuf/empty.ts";
 import { type Bot, createBot } from "mineflayer";
+import type { Logger } from "pino";
+
+function wrap<Args extends any[], Return>(fn: (...args: Args) => Promise<Return>): (...args: Args) => Promise<Return> {
+    return async (...args) => {
+        try {
+            return await fn(...args);
+        } catch (e: any) {
+            throw new ServerError(Status.INTERNAL, e.toString());
+        }
+    };
+}
 
 export class Rpc implements ServiceImplementation<BotDefinition> {
     private bot?: Bot;
 
     private config;
-    constructor(config: any) {
+    private log: Logger;
+
+    constructor(config: any, log: Logger) {
         this.config = config;
+        this.log = log;
     }
 
-    async ping(_request: Empty, _context: CallContext): Promise<DeepPartial<PingResponse>> {
-        return { payload: "Pong!" };
-    }
+    ping = wrap(
+        async (_request: Empty, _context: CallContext): Promise<DeepPartial<PingResponse>> => ({ payload: "Pong!" }),
+    );
 
     async *streamEvents(_request: Empty, _context: CallContext): AsyncIterable<DeepPartial<Event>> {}
 
-    async connect(request: ConnectRequest, context: CallContext): Promise<Empty> {
+    connect = wrap(async (request: ConnectRequest, _context: CallContext): Promise<Empty> => {
         this.bot = createBot({
             host: request.host,
             port: request.port,
@@ -27,5 +41,9 @@ export class Rpc implements ServiceImplementation<BotDefinition> {
         });
 
         return {};
+    });
+
+    close() {
+        this.log.info("Closing");
     }
 }
